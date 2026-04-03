37ba12fdde3b9f0b555481f264cf5b2a979b8013
Stefan Moog <moogs@gmx.de>
2026-04-03 12:08:39 +0200

    Initial commit

---
+++ src/Barplot.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import * as d3 from "d3";
+import "./Barplot.css";
+import "./theme.css";
+
+// Initialize React Component
+function Barplot({ data, width, height }) {
+  // 1️⃣ Do some math with D3
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.students)])
+    .range([0, width - 175]);
+  const yScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.country))
+    .range([0, height - 100]);
+  const barHeight = 25;
+  const barColor = "var(--blue)";
+
+  const offsetx = 130;
+  const offsety = 95;
+
+  const axisMargin = 5;
+  const toHighlight = new Set(["Switzerland", "Ireland", "Singapore"]);
+
+  const ySource = offsety + 19 * (barHeight + 5) - 5 - barHeight / 2 + 10;
+
+  // 2️⃣ Render with React (JSX!) using the D3 maths
+  return (
+    <svg width={width} height={height} className="barplot">
+      <rect x={0} y={0} width={width} height={height} className="plot-bg" />
+      <text x={5} y={25} className="title">
+        Which countries punch above their weight in student representation?
+      </text>
+      <text x={5} y={50} className="subtitle">
+        <tspan dy={0}>
+          Although the United States accounts for the largest number of students
+          in absolute terms,
+        </tspan>
+        <tspan dy="1.2em" x={5}>
+          <tspan font-weight="bold" fill="var(--green)">
+            Ireland
+          </tspan>
+          ,{" "}
+          <tspan font-weight="bold" fill="var(--red)">
+            Switzerland
+          </tspan>
+          , and{" "}
+          <tspan font-weight="bold" fill="var(--darkblue)">
+            Singapore
+          </tspan>{" "}
+          stand out most on a per-capita basis.
+        </tspan>
+      </text>
+      {data.map((d, i) => {
+        const x = xScale(d.students);
+        const yLabel =
+          offsety + (i + 1) * (barHeight + 5) - 5 - barHeight / 2 + 4;
+        const yBar = offsety + i * (barHeight + 5);
+
+        const align = d.students < 30 ? "left" : "right";
+        const textOffset = d.students < 30 ? 5 : -5;
+
+        return (
+          <g key={i}>
+            <text
+              key={"x-" + i}
+              x={axisMargin}
+              y={yLabel}
+              className="xAxisText"
+            >
+              {d.country}
+            </text>
+            <rect
+              key={"bar-" + i}
+              x={offsetx}
+              y={yBar}
+              width={xScale(d.students)}
+              height={barHeight}
+              fill={d.color}
+              className="bar"
+            />
+            <text
+              key={"label-" + i}
+              x={offsetx + textOffset + xScale(d.students)}
+              y={yLabel}
+              className={`label ${align}`}
+            >
+              {d.students}
+              {i === 0 ? " students" : ""} ({d.students_per_million}
+              {i === 0 ? " students per million inhabitants" : ""})
+            </text>
+          </g>
+        );
+      })}
+
+      <text x={5} y={ySource} className="source">
+        Data Source: Yan Holtz (student numbers),{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population_%28United_Nations%29"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <tspan class="svg-link">Wikipedia</tspan>
+        </a>{" "}
+        (population numbers)
+      </text>
+    </svg>
+  );
+}
+
+export default Barplot;
---
+++ src/main.jsx
@@ -0,0 +1,53 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Barplot from "./Barplot.jsx";
+
+const data = [
+  { country: "United States", students: 68, population: 343477335 },
+  { country: "France", students: 21, population: 66438822 },
+  { country: "United Kingdom", students: 21, population: 68682962 },
+  { country: "Germany", students: 20, population: 84548231 },
+  { country: "Switzerland", students: 13, population: 8870561 },
+  { country: "Spain", students: 10, population: 47911579 },
+  { country: "Netherlands", students: 9, population: 18092524 },
+  { country: "India", students: 9, population: 1438069596 },
+  { country: "Singapore", students: 8, population: 5789090 },
+  { country: "Ireland", students: 8, population: 5196630 },
+  { country: "Sweden", students: 7, population: 10551494 },
+  { country: "Australia", students: 7, population: 26451124 },
+  { country: "Canada", students: 6, population: 39299105 },
+  { country: "Finland", students: 5, population: 5601185 },
+  { country: "Mexico", students: 4, population: 129739759 },
+  { country: "Brazil", students: 4, population: 211140729 },
+  { country: "Saudi Arabia", students: 3, population: 32264292 },
+  { country: "Romania", students: 3, population: 19118479 },
+  { country: "Philippines", students: 3, population: 114891199 },
+  { country: "New Zealand", students: 3, population: 5172836 },
+];
+
+const colorMap = {
+  Switzerland: "var(--red)",
+  Ireland: "var(--green)",
+  Singapore: "var(--darkblue)",
+};
+
+const transformedData = data
+  .map((d) => ({
+    ...d,
+    students_per_million: Number(
+      (d.students / (d.population / 1_000_000)).toFixed(2),
+    ),
+    color: colorMap[d.country] ?? "var(--blue)",
+  }))
+  .filter((d, i) => i < 18);
+
+// The chart dimensions (often passed as prop too)
+const width = 800;
+const height = 1600;
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Barplot data={transformedData} width={width} height={height} />
+  </StrictMode>,
+);
